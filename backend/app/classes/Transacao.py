de56706d6944bf47a4b4f4f05942fc9e35c310d3
Rodolfaooo <pedroesmeraldooffice@gmail.com>
2026-09-23 00:24:56 -0300

    Push Transação

--- backend/app/classes/Transacao.py
+++ backend/app/classes/Transacao.py
@@ -25,74 +25,72 @@ class Transacao(ABC):
         return self._pagador
 
     @pagador.setter
-    def pagador(self):
-        self._pagador = self.pagador
+    def pagador(self, valor):
+        self._pagador = valor
 
     @property
     def data(self):
         return self._data
     
     @data.setter
-    def data(self):
-        self._data = self.data
+    def data(self, valor):
+        self._data = valor
 
     @property
     def horario(self):
         return self._horario
     
     @horario.setter
-    def horario(self):
-        self._horario = self.horario
+    def horario(self, valor):
+        self._horario = valor
 
     @property
     def valor(self):
         return self._valor
 
     @valor.setter
-    def valor(self):
-        self._valor = self.valor
+    def valor(self, valor):
+        self._valor = valor
 
     @property
     def metodo_pagamento(self):
         return self._metodo_pagamento
     
     @metodo_pagamento.setter
-    def metodo_pagamento(self):
-        self._metodo_pagamento = self.metodo_pagamento
+    def metodo_pagamento(self, valor):
+        self._metodo_pagamento = valor
 
     @property
     def data_vencimento(self):
         return self._data_vencimento
     
     @data_vencimento.setter
-    def data_vencimento(self):
-        self._data_vencimento = self.data_vencimento
+    def data_vencimento(self, valor):
+        self._data_vencimento = valor
 
     @property
     def data_pagamento(self):
         return self._data_pagamento
 
     @data_pagamento.setter
-    def data_pagamento(self):
-        self._data_pagamento = self.data_pagamento
+    def data_pagamento(self, valor):
+        self._data_pagamento = valor
 
     @property
     def remetente(self):
         return self._remetente
     
     @remetente.setter
-    def remetente(self):
-        self._remetente = self.remetente
+    def remetente(self, valor):
+        self._remetente = valor
 
     @property
     def categoria(self):
         return self._categoria
     
     @categoria.setter
-    def categoria(self):
-        self._categoria = self.categoria
-
-
+    def categoria(self, valor):
+        self._categoria = valor
 
 class Receita(Transacao):
     def __init__(self, pagador, *, id, data, horario, valor, metodo_pagamento, data_vencimento, data_pagamento, remetente):
@@ -104,8 +102,8 @@ class Receita(Transacao):
         return self._pagador
 
     @pagador.setter
-    def pagador(self):
-        self._pagador = self.pagador
+    def pagador(self, valor):
+        self._pagador = valor
 
 class Despesa(Transacao):
     def __init__(self, recebedor, *, id, data, horario, valor, metodo_pagamento, data_vencimento, data_pagamento, remetente):
@@ -117,5 +115,5 @@ class Despesa(Transacao):
         return self._recebedor
 
     @recebedor.setter
-    def recebedor(self):
-        self._recebedor = self.recebedor
+    def recebedor(self, valor):
+        self._recebedor = valor
